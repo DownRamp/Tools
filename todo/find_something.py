a6434fe335c1edcdde51fd878758b2d663bee0a5
Down Ramp Apps <98784052+DownRamp@users.noreply.github.com>
2022-05-31 20:42:41 -0400

    refactor and added possible ideas

---
+++ todo/find_something.py
@@ -0,0 +1,8 @@
+# pick two images
+# find color closest to image in background image (suggest location)
+# allow user to drag image
+# blur lines of hider image
+# set size of initial image (hider)
+# combine images
+# Show preview
+# create image
